b370f96c746b0465a8067edf637d517782047763
Pocha <pocha.sharma@gmail.com>
2013-01-16 13:08:11 +0000

    add todo using form_for

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,19 +1,21 @@
 class TodosController < ApplicationController
   def index
       @todo_items = Todo.all
+    	@todo = Todo.new
   end
 
   def delete
     Todo.last.delete
-   
   end
+  
   def add
-    todo = Todo.create(:todo_item => params[:todo_text])
-    if !todo.valid?
-        flash[:error] = todo.errors.full_messages.join("<br>").html_safe
-   end
-
-   redirect_to :action => 'index'
+  @todo = Todo.new(params[:todo])
+    if @todo.save
+      flash[:success] = "Todo added successfully"
+    else
+     flash[:error] = @todo.errors.full_messages.join("<br>").html_safe
+    end
+  redirect_to :action => 'index'
   end
 
 end
