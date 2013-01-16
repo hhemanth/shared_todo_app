adc1d30dddf265075c11b487f5434949d64455f8
Pocha <pocha.sharma@gmail.com>
2013-01-16 08:32:56 +0000

    display 1 todo

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,6 @@
+class TodosController < ApplicationController
+  def index
+      @todo_item1 = "Buy Milk"
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShareTodoApp::Application.config.secret_token = '75f8caa4ca355da170994f92671aaf8a202a3395dc701a297d34f4ad63bd42173069495438aabb94fa9cbcfa880e090aea6e96e4b65bc758fbe9d3a3d7220453'
